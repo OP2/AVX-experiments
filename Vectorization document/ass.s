81177a7265dc6bb1064a3a42423479c8bfa24978
Alex Appetiti <alzamos@gmail.com>
2012-10-13 12:50:02 +0200

    Update completed

--- Vectorization document/ass.s
+++
@@ -1,3 +0,0 @@
-vdivpd    %ymm2, %ymm1, %ymm0                           #1556.5
-vmulpd    %ymm3, %ymm0, %ymm12                          #1558.7
-vmulpd    %ymm12, %ymm12, %ymm13                        #1561.7
